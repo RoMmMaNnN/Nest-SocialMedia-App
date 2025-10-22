77067079417b0235dbc778d96566d5d74c51f3be
Roman Kriuchkov <157597992+RoMmMaNnN@users.noreply.github.com>
2025-10-22 22:27:25 +0100

    Added providers to AppModule to fix error 404 on root page

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service'; // <-- додати
 
 @Module({
   imports: [
@@ -24,5 +25,6 @@ import { AppController } from './app.controller';
     UsersModule,
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
