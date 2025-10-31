bca6bfe4bdbc78fc83032cc791a487681a838436
Roman Kriuchkov <157597992+RoMmMaNnN@users.noreply.github.com>
2025-10-31 20:58:24 +0000

    Final seeder changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '../modules/database/database.module';
 import { UsersModule } from '../modules/users/users.module';
 import { AuthModule } from '../modules/auth/auth.module';
 import { PostsModule } from '../modules/posts/posts.module';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
+import { SeederService } from '../modules/database/services/seeder.service';
 
 @Module({
   imports: [ConfigModule, DatabaseModule, UsersModule, AuthModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SeederService],
 })
 export class AppModule {}
