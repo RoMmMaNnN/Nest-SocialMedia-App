c491b5e58e53b1eb0fa6cefd412cb4fbbeb98318
Roman Kriuchkov <157597992+RoMmMaNnN@users.noreply.github.com>
2025-10-31 16:17:02 +0000

    Added post first logic

--- src/modules/posts/controllers/posts.controller.ts
+++ src/modules/posts/controllers/posts.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
+import { PostsService } from '../services/posts.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { QueryPostsDto } from '../dto/query-posts.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll(@Query() query: QueryPostsDto) {
+    return this.postsService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreatePostDto) {
+    return this.postsService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() dto: UpdatePostDto) {
+    return this.postsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.postsService.remove(id);
+  }
+}
--- src/modules/posts/services/posts.service.ts
+++ src/modules/posts/services/posts.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ILike, Repository } from 'typeorm';
+import { Post } from '../entities/post.entity';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { UsersService } from '../../users/services/users.service';
+import { QueryPostsDto } from '../dto/query-posts.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepo: Repository<Post>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async findAll(query?: QueryPostsDto) {
+    const qb = this.postRepo
+      .createQueryBuilder('post')
+      .leftJoinAndSelect('post.author', 'author');
+
+    if (query?.authorId) {
+      qb.andWhere('author.id = :authorId', { authorId: query.authorId });
+    }
+
+    if (query?.search) {
+      qb.andWhere('post.title ILIKE :search OR post.content ILIKE :search', {
+        search: `%${query.search}%`,
+      });
+    }
+
+    if (query?.sort) {
+      qb.orderBy('post.createdAt', query.sort);
+    } else {
+      qb.orderBy('post.createdAt', 'ASC');
+    }
+
+    if (query?.limit) {
+      qb.take(query.limit);
+    }
+
+    if (query?.offset) {
+      qb.skip(query.offset);
+    }
+
+    return qb.getMany();
+  }
+
+  async findOne(id: number) {
+    const post = await this.postRepo.findOne({
+      where: { id },
+      relations: ['author'],
+    });
+    if (!post) throw new NotFoundException('Post not found');
+    return post;
+  }
+
+  async create(dto: CreatePostDto) {
+    const author = await this.usersService.findOne(dto.authorId);
+    const post = this.postRepo.create({ ...dto, author });
+    return this.postRepo.save(post);
+  }
+
+  async update(id: number, dto: UpdatePostDto) {
+    const post = await this.findOne(id);
+    if (dto.authorId) {
+      post.author = await this.usersService.findOne(dto.authorId);
+    }
+    Object.assign(post, dto);
+    return this.postRepo.save(post);
+  }
+
+  async remove(id: number) {
+    const post = await this.findOne(id);
+    await this.postRepo.remove(post);
+    return { message: `Post ${id} deleted` };
+  }
+}
