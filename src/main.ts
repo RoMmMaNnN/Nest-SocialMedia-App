9f2414cf27def994c123a1a599f82cccb8929317
Roman Kriuchkov <157597992+RoMmMaNnN@users.noreply.github.com>
2025-10-31 21:11:23 +0000

    seeder implemented interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import cookieParser from 'cookie-parser';
 import { SeederService } from './modules/database/services/seeder.service';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   app.use(cookieParser());
   app.useGlobalPipes(
